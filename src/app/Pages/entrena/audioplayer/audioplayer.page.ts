5779bef84f83ad3f4125d7f7cdb42acadecb58dc
AValeriaCM <anvalecasmo@outlook.es>
2021-01-04 16:19:21 -0500

    ajustes audioplayer

--- src/app/Pages/entrena/audioplayer/audioplayer.page.ts
+++ src/app/Pages/entrena/audioplayer/audioplayer.page.ts
@@ -64,7 +64,7 @@ export class AudioplayerPage implements OnInit {
         this.updateProgress();
       },
       onend: () => {
-
+        this.player.stop();
       }
     });
     this.player.play();
